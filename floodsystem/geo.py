f513671e1d4f6e4e3f92d0e51b1fc2c617ee00c5
Kelvin-kfl33 <98097753+Kelvin-kfl33@users.noreply.github.com>
2022-02-07 22:11:29 +0000

    Update geo.py

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,20 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine, Unit
+
+def stations_by_distance(stations, p):
+    stations_name = []
+    stations_towns = []
+    stations_distance = []
+    for station in stations:
+        distance = haversine(p, station.coord)
+        if station is not stations:
+            stations_name.append(station.name)
+            stations_towns.append(station.town)
+            stations_distance.append(distance)
+        else:
+            pass
+    tuples = list(zip(stations_name, stations_towns, stations_distance))
+    tuples = sorted_by_key(tuples, 2)
+    return tuples
