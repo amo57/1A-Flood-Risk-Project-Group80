8f5df0a893cccd7e22996348180ed82db4b2e53b
Kelvin-kfl33 <98097753+Kelvin-kfl33@users.noreply.github.com>
2022-02-10 10:42:01 +0000

    Update Task1E.py

--- Task1E.py
+++ Task1E.py
@@ -5,5 +5,7 @@ def run():
     stations = build_station_list()
     x = rivers_by_station_number(stations, 9)  ## shows rivers with top 9 number of stations, not the top 9 position in the river list
     print(x)
+    """Test to make sure that the number of stations in any river are less than or equal to the maximum number of stations, i.e. 54"""
+    assert x[N][1] <= 54
 
 run()
